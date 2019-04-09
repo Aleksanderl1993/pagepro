806d71a1278f77a172b05e3f47457447965ee35f
Aleksander <>
2019-04-09 21:58:02 +0200

    Happening now section in progress.

--- src/libs/script.js
+++ src/libs/script.js
@@ -1,9 +1,24 @@
 console.log('haha');
 
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('#news .swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 });
 
+var swiperTrending = new Swiper('#trending .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
